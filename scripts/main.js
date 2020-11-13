38d23242fdfb33bf4834bb3d079415d98f9af1b3
littlett98 <37678140+littlett98@users.noreply.github.com>
2020-11-13 15:59:05 -0500

    switched selection to querySelector and mad more efficient code

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,20 @@
 "use strict";
 
-let article = document.getElementById('quote');
+let article = document.querySelector('#quote');
 
 //Starting point is the setup when event listeners are attached
 document.addEventListener("DOMContentLoaded", setup);
 
 function setup() {
     //find the button and give it a click listener
-    let quoteButton = document.getElementById('quote-button');
+    let quoteButton = document.querySelector('#quote-button');
     quoteButton.addEventListener("click", getQuote);
 }
 
 function getQuote() {
-    fetch('https://ron-swanson-quotes.herokuapp.com/v2/quote')
+    fetch('https://ron-swanson-quotes.herokuapp.com/v2/quotes')
         .then(response => {
             if (!response.ok) {
-
                 throw new Error('Status code: ' + response.status)
             }
             return response.json();
@@ -23,7 +22,7 @@ function getQuote() {
         .then(json => addQuote(json))
         .catch( error => {
             console.error('There was a problem: '  + error);
-            getQuoteError();
+            addQuoteError();
         });
 }
 
@@ -39,7 +38,7 @@ function removeOldQuote() {
     article.setAttribute("id", "quote");
 }
 
-function getQuoteError() {
+function addQuoteError() {
     removeOldQuote();
     let text = document.createTextNode("Error");
     article.appendChild(text);
